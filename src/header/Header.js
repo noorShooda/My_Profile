e54a9e0bd0f2d9dddf4ed9a0ce30df0eb9f5a59c
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-26 17:24:56 +0200

    feat: add the commonent Navigation to the Header Component and adjust the CSS style to the featur needs.

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { BsFillHouseFill, BsFillFolderFill } from 'react-icons/bs';
-import { AiFillMail } from 'react-icons/ai';
 
-import { rootingURL } from '../config/rootingURL';
 import  './header.css';
 import image from './myPhoto.jpg';
-
-
-
+import { Navigation } from './navigation/Navigation';
 export const Header =function(){
     
     return(
         <div className="headerStyle">
             <div className="profileImgStyle"><img class="imageRoundedStyle" src={image} alt="nour" height="100" width="100"/></div>
-            <ul class="listStyle">
-            <li class="listItemStyle"><BsFillHouseFill/><Link className="headerItemStyle" to={rootingURL.home}>Home</Link></li>
-            <li class="listItemStyle"><BsFillFolderFill/><Link className="headerItemStyle" to={rootingURL.portfolio}>Portfolio</Link></li>
-            <li class="listItemStyle"><AiFillMail/><Link className="headerItemStyle" to={rootingURL.contact}>Contact</Link></li>
-            </ul>
+            <Navigation/>
         </div>
     );
 }
