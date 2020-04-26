8a3f07048c864c677f2bd8e335028bd13bb5d1f9
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-26 17:06:57 +0200

    feat: add new component to the navigation list . Add new photos in the sidebar and adjust the CSS style to the needs of the feature.

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsFillHouseFill, BsFillFolderFill } from 'react-icons/bs';
+import { AiFillMail } from 'react-icons/ai';
 
 import { rootingURL } from '../config/rootingURL';
 import  './header.css';
+import image from './myPhoto.jpg';
+
+
 
 export const Header =function(){
+    
     return(
         <div className="headerStyle">
-            <p className="headerItemStyle"><Link to={rootingURL.home}>Home</Link></p>
-            <p className="headerItemStyle"><Link to={rootingURL.contact}>Contact</Link></p>
+            <div className="profileImgStyle"><img class="imageRoundedStyle" src={image} alt="nour" height="100" width="100"/></div>
+            <ul class="listStyle">
+            <li class="listItemStyle"><BsFillHouseFill/><Link className="headerItemStyle" to={rootingURL.home}>Home</Link></li>
+            <li class="listItemStyle"><BsFillFolderFill/><Link className="headerItemStyle" to={rootingURL.portfolio}>Portfolio</Link></li>
+            <li class="listItemStyle"><AiFillMail/><Link className="headerItemStyle" to={rootingURL.contact}>Contact</Link></li>
+            </ul>
         </div>
     );
 }
