bd2d2c5a0329f118c004ba1c12a266bc89b4b8fc
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-26 19:35:44 +0200

    feat: create a local state and add a lifecycle method to get my Mocked Data plus using the component ListDisplayer to display my whole list.

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 import './portfolio.css';
+import { ListDisplayer } from './listDisplayer/ListDisplayer';
+import { getLanguagePortfolio } from '../client/client'
+
 class Portfolio extends React.Component{
 
+    state={languagePortfolio:[]};
+
+    componentDidMount(){
+        const languagePortfolio = getLanguagePortfolio();
+        this.setState({languagePortfolio}); 
+    }
+   
     render(){
         return(
-            <p className="portfolioStyle">Hello Portfolio</p>
+            <div className="portfolioStyle">
+            <p>Hello Portfolio</p>
+            <ListDisplayer languagePortfolio={this.state.languagePortfolio}/>
+            </div>
         )
     }
 }
