9e7c75e0198ff01439bad4ee421e798532546ae3
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-26 16:59:03 +0200

    feat: create the component Portfolio plus the suitble CSS style to it .

---
+++ src/portfolio/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './portfolio.css';
+class Portfolio extends React.Component{
+
+    render(){
+        return(
+            <p className="portfolioStyle">Hello Portfolio</p>
+        )
+    }
+}
+export {Portfolio};
