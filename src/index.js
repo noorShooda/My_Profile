f3634a601734edbdb1a07b861b67bd7589106c75
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-28 17:35:47 +0200

    feat: wrap the main component  with provider component from react-redux to be able to use redux store.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { render } from 'react-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import { App } from './App';
+import { stateStore } from './store';
 
 render(
-    <App/>,
+    <Provider store={stateStore}>
+         <App/>
+    </Provider>,
     document.getElementById('root')
 );
