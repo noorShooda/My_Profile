6ea3dffa053b564a4f87e018ecd106f48d85ef02
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-28 15:29:05 +0200

    feat: use axios to get the data from the endpoint .

--- src/client/client.js
+++ src/client/client.js
@@ -1,4 +1,10 @@
-export const getLanguagePortfolio = ()=>{
-    const response = require('../client/data.json');
-    return(response.languagePortfolio);
+import axios from 'axios';
+
+export const getLanguagePortfolio = async ()=>{
+    const response = await axios.get('http://localhost:4000/languagePortfolio') ;
+    return(response.data);
+}
+export const getPortfolioTools = async ()=>{
+    const response = await axios.get('http://localhost:4000/portfolioTools');
+    return(response.data);
 }
