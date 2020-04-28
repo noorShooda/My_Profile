41c4fb59f85722cae7cad3ef97718f9542c10c64
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-28 17:33:15 +0200

    feat: setup of the redux store.

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers }from 'redux';
+import { protfolioReducer } from './portfolio/portfolioReducer';
+
+export const stateStore = createStore(
+    combineReducers({portfolioState:protfolioReducer})
+    
+);
