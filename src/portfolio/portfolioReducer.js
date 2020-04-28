451838c683b6d4f6a45f9c55b9218c536ab14fe8
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-28 17:40:03 +0200

    feat: add portfolio Reducer.

---
+++ src/portfolio/portfolioReducer.js
@@ -0,0 +1,24 @@
+import { GET_LANGUAGE, GET_LANGUAGE_FAIL } from './actions';
+
+const initialState = {languagePortfolio:[],languagePortfolioIsError:false}
+export const protfolioReducer=(state = initialState, action)=>{
+    switch(action.type){
+        case GET_LANGUAGE:{
+            return({languagePortfolio:action.payload, languagePortfolioIsError: state.languagePortfolioIsError})
+        }
+        case GET_LANGUAGE_FAIL:{
+            return(
+                { languagePortfolio: state.languagePortfolio, languagePortfolioIsError: action.payload
+                }
+            )
+        }
+        default:{
+            return(
+                {
+                    languagePortfolio: state.languagePortfolio, languagePortfolioIsError: state.languagePortfolioIsError
+                }
+            )
+        }
+    }
+
+}
