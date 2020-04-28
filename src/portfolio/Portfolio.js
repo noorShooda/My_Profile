0364f3a3ded07ea99b8d245921374731798ed7d7
noorShooda <sebai.noorelhooda@gmail.com>
2020-04-28 15:38:13 +0200

    refactor: adapt the code to use Promise patterns.

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -2,22 +2,36 @@ import React from 'react';
 
 import './portfolio.css';
 import { ListDisplayer } from './listDisplayer/ListDisplayer';
-import { getLanguagePortfolio } from '../client/client'
+import { getLanguagePortfolio, getPortfolioTools } from '../client/client'
 
 class Portfolio extends React.Component{
 
-    state={languagePortfolio:[]};
+    state={languagePortfolio:[],portfolioTools:[], languagePortfolioIsError:false,portfolioToolsIsError:false};
 
     componentDidMount(){
-        const languagePortfolio = getLanguagePortfolio();
-        this.setState({languagePortfolio}); 
+        getLanguagePortfolio()
+        .then(
+            (response)=>{this.setState({languagePortfolio: response});},
+            ()=>{
+                this.setState({languagePortfolioIsError:true});
+            }
+        );
+
+        getPortfolioTools()
+        .then(
+            (response)=>{this.setState({portfolioTools: response});},
+            ()=>{
+                this.setState({portfolioToolsIsError:true});
+            }
+        );
     }
    
     render(){
         return(
             <div className="portfolioStyle">
-            <p>Hello Portfolio</p>
+            <p className="pageHeader">Hello Portfolio</p>
             <ListDisplayer languagePortfolio={this.state.languagePortfolio}/>
+            <p>{this.state.portfolioTools.join('-')}</p>
             </div>
         )
     }
