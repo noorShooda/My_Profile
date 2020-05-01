e55d1e68d73589107d1c4155a1ad2eb50ff2c65d
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-01 20:04:31 +0200

    feat: -create the update actions in the action creator. -update the reducer to handle the update logic.

--- src/portfolio/actions.js
+++ src/portfolio/actions.js
@@ -1,6 +1,9 @@
 export const GET_LANGUAGE ="getPortfolioLanguage";
 export const GET_LANGUAGE_FAIL="getPortfolioLanguageFail";
 export const GET_LANGUAGE_SUC="getPortfolioLanguageSuccess";
+export const UPDATE_LANGUAGE_FAIL="updatePortfolioLanguageFail";
+export const UPDATE_LANGUAGE_REQUEST="updatePortfolioLanguageRequest";
+export const UPDATE_LANGUAGE_SUC="updatePortfolioLanguageSuccess";
 
 export const getPortfolioLanguage = () => {
     return(
@@ -26,4 +29,34 @@ export const getPortfolioLanguageSuccess = (data) => {
             payload: data   
         }
     );
+};
+
+export const updatePortfolioLanguageRequest = (id,data) =>{
+    return(
+        {
+            type: UPDATE_LANGUAGE_REQUEST,
+            payload:{
+                id: id,
+                data: data
+            }
+        }
+    )
+};
+
+export const updatePortfolioLanguageSuccess = (data) =>{
+    return(
+        {
+            type: UPDATE_LANGUAGE_SUC,
+            payload:data
+        }
+    )
+};
+
+export const updatePortfolioLanguageFail = (error) =>{
+    return(
+        {
+            type: UPDATE_LANGUAGE_FAIL,
+            payload:error
+        }
+    )
 };
