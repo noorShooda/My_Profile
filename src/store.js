81557d8dbd49ebbfe07d5e6876a105193182c551
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-01 15:55:14 +0200

    feat: set up the store to use saga.

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
-import { createStore, combineReducers }from 'redux';
-import { protfolioReducer } from './portfolio/portfolioReducer';
+import { createStore, combineReducers, applyMiddleware }from 'redux';
+import  createSagaMiddleware from 'redux-saga';
 
+import { protfolioReducer } from './portfolio/portfolioReducer';
+import { getPortfolioLanguageListner } from './portfolio/saga';
+
+const sagaMiddleware = createSagaMiddleware();
 export const stateStore = createStore(
-    combineReducers({portfolioState:protfolioReducer})
+    combineReducers({portfolioState:protfolioReducer}),
+    applyMiddleware(sagaMiddleware)
 );
+sagaMiddleware.run(getPortfolioLanguageListner);
