4e60dd82b87852242dadf54ea06b4a58922e1982
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-01 20:02:06 +0200

    feat: create update request.

--- src/client/client.js
+++ src/client/client.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
-export const getLanguagePortfolio = async ()=>{
+export const getLanguagePortfolio = async () => {
     const response = await axios.get('http://localhost:4000/languagePortfolio') ;
     return(response.data);
 }
-export const getPortfolioTools = async ()=>{
+export const getPortfolioTools = async () => {
     const response = await axios.get('http://localhost:4000/portfolioTools');
     return(response.data);
+}
+export const updatePrtfolioLanguages = async (id, change) => {
+    await axios.put(`http://localhost:4000/languagePortfolio/${id}`,change);
 }
