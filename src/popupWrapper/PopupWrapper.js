0e43078c102b6d0f07abb1ce9bbf434ccd7d3c58
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-01 22:44:45 +0200

    refactor: change the popup wrapper from a functional component to a class component.

--- src/popupWrapper/PopupWrapper.js
+++ src/popupWrapper/PopupWrapper.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './popupWrapper.css';
 
-export const PopupWrapper = (props) => {
+class PopupWrapper extends React.Component {
 
-    const [language, setLanguage]=useState(props.language);
-
-    const changeValue =(e) => {
-        setLanguage(e.target.value);
+    constructor(props){
+        super(props);
+        this.changeValue=this.changeValue.bind(this);
+        this.updateValue=this.updateValue.bind(this);
     }
-    const updateValue = () =>{
-        props.update(props.id,{
-            language: language,
-            numberOfProject: props.numberOfProject,
+
+    state = { language: this.props.language};
+
+    changeValue(e) {
+        this.setState({language: e.target.value});
+    }
+    updateValue(){
+        this.props.update(this.props.id,{
+            language: this.state.language,
+            numberOfProject: this.props.numberOfProject,
             duration: {
-                from: props.from,
-                to: props.to
+                from: this.props.from,
+                to: this.props.to
             }
         });
-        props.onClose();
+        this.props.onClose();
     }
-    
-    return(
-        <div className="overlay">
-            <div className="popup">
-                <p>Language: <input value={language} size="5" onChange={changeValue}/></p>
-                <p>Number of Projects: {props.numberOfProject}</p>
-                <button onClick={updateValue}disabled={language === props.language}>Save</button>
-                <button onClick={props.onClose}>Close Popup</button>
+    render(){
+        return(
+            <div className="overlay">
+                <div className="popup">
+                    <p>Language: <input value={this.state.language} size="5" onChange={this.changeValue}/></p>
+                    <p>Number of Projects: {this.props.numberOfProject}</p>
+                    <button onClick={this.updateValue}disabled={this.state.language === this.props.language}>Save</button>
+                    <button onClick={this.props.onClose}>Close Popup</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+export {PopupWrapper};
