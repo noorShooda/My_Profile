a048dbc665563b1e4e49f2ed34ecd799caf7cd4e
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-01 16:14:44 +0200

    fix: remove console logs and fix code style.

--- src/portfolio/saga.js
+++ src/portfolio/saga.js
@@ -1,19 +1,17 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
 
 import { getLanguagePortfolio } from '../client/client';
-import { GET_LANGUAGE, getPortfolioLanguageSuccess, getPortfolioLanguageFail} from './actions';
+import { GET_LANGUAGE, getPortfolioLanguageSuccess, getPortfolioLanguageFail } from './actions';
 
-function * getPortfolioLanguage(){
+function * getPortfolioLanguage() {
     try {
-        console.log("saga getData called");
         const response = yield call(getLanguagePortfolio);
-        console.log("saga getData",response);
         yield put(getPortfolioLanguageSuccess(response));
     } catch (error) {
-        console.error(error);
         yield put(getPortfolioLanguageFail(true));
     }
 }
-export function * getPortfolioLanguageListner(){
-yield takeLatest(GET_LANGUAGE, getPortfolioLanguage);
+
+export function * getPortfolioLanguageListner() {
+    yield takeLatest(GET_LANGUAGE, getPortfolioLanguage);
 }
