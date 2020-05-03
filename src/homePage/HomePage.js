d35cd5080040e684089650aca96976a32939eda9
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-03 14:49:11 +0200

    feat: -implement the ListDisplayer component in the home page. -change the homePage component to a class component. -update the CSS style for the new display .

--- src/homePage/HomePage.js
+++ src/homePage/HomePage.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './homePage.css';
 
+import { getStudiesPathRequest } from './actions';
+import { ErrorHandler } from '../errorHandler/ErrorHandler';
+import { ListDisplayer } from './listDisplayer/ListDisplayer';
+
 class HomePage extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.displayStudiesPath=this.displayStudiesPath.bind(this);
+    }
+
+    componentDidMount(){
+       this.props.getStudiesPath(); 
+    }
+    displayStudiesPath() {
+        if (this.props.studiesPathsIsError){
+           return <ErrorHandler error="There was an error in the get portfolio languages !"/>;
+        }
+        else {
+            return(
+                <ListDisplayer 
+                    path={this.props.studiesPaths} 
+                    
+                />
+            );
+        }
+    }
     render(){
         return(
-            <p className="homePageStyle">Hello react</p>
+            <div className="homePageStyle">
+                {this.displayStudiesPath()}
+            </div>
         );
     }
 }
-export {HomePage};
+const mapActionToProps = {
+    getStudiesPath: getStudiesPathRequest
+}
+const mapStateToProps = (state) => {
+    return(
+        {
+            studiesPaths: state.homePageState.studiesPaths,
+            studiesPathsIsError: state.homePageState.studiesPathsIsError
+        }
+    )
+}
+export default connect(mapStateToProps,mapActionToProps)(HomePage);
