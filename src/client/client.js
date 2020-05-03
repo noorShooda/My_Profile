ad21ebe3eb1cc921c0f0b7e240076ea05e276bf5
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-03 15:00:01 +0200

    feat: add the getStudiespath function to get the data from the endpoint.

--- src/client/client.js
+++ src/client/client.js
@@ -11,3 +11,7 @@ export const getPortfolioTools = async () => {
 export const updatePrtfolioLanguages = async (id, change) => {
     await axios.put(`http://localhost:4000/languagePortfolio/${id}`,change);
 }
+export const getStudiesPath = async () => {
+    const response = await axios.get('http://localhost:4000/studiesPaths') ;
+    return(response.data);
+}
