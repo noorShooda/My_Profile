0e37a5878bbb00f3050ea05adfd31adbfe35e1c5
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-03 16:06:34 +0200

    refactor:-convert the form component to a functional component using the useState hook.

--- src/contact/contactForm/Form.js
+++ src/contact/contactForm/Form.js
@@ -1,51 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './form.css';
 
-class Form extends React.Component {
-    constructor() {
-        super();
-        this.handleChange=this.handleChange.bind(this);
-        this.handleButtonChange=this.handleButtonChange.bind(this);
-    }
+export const Form = () => {
+    
+    const [formElement, setFormElement] = useState({
+        name: "",
+        last_name: "",
+        age: "", 
+        topic: "",
+        note: ""
+    });
+    
+   const handleChange = (event) => {
+        const nam = event.target.name;
+        const val = event.target.value;
+        const newFormElement = {...formElement,[nam]: val};
+        setFormElement(newFormElement);
+    };
 
-    state = {
-        name:"",
-        last_name:"",
-        age:null, 
-        topic:"",
-        note:""
-    }
+    const handleSubmitForm = (event) => {
+        event.preventDefault();
+        setFormElement({
+            name: "",
+            last_name: "",
+            age: "", 
+            topic: "",
+            note: ""
+        });
+    };
 
-    handleChange(event) {
-        let nam = event.target.name;
-        let val = event.target.value;
-        this.setState({[nam]: val});
-    }
-    handleButtonChange() {
-        this.setState({name:"test",last_name:"",age:null, topic:"",note:""});
-    }
-
-    render() {
-        return(
-            <form className="formStyle">
-                        <label for="fname">First name:</label><br/>
-                        <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/><br/>
-                        <label for="lname">Last name:</label><br/>
-                        <input type="text" name="last_name" value={this.state.last_name} onChange={this.handleChange}/><br/>
-                        <label for="age">Age:</label><br/>
-                        <input type="text" name="age" value={this.state.age} onChange={this.handleChange}/><br/>
-                        <label for="topic">Choose a topic :</label><br/>
-                        <select id="topics" name="topic" onSelect={this.handleChange}>
-                                <option value="personalContact">Contact for personal affairs</option>
-                                <option value="contactForBug">Contact for a bug</option>
-                                <option value="contactForrecruitment">Contact for Recruitment</option>
-                        </select><br/>
-                        <label for="note">Note:</label><br/>
-                        <textarea name="note" rows="10" cols="30" value={this.state.note} onChange={this.handleChange}/><br/>
-                        <button onClick={this.handleButtonChange}>Submit</button>
-                        </form>                
-        )
-    }
-}
-export {Form};
+    return(
+        <form className="formStyle" onSubmit={handleSubmitForm }>
+            <label for="fname">First name:</label><br/>
+            <input type="text" name="name" value={formElement.name} onChange={handleChange}/><br/>
+            <label for="lname">Last name:</label><br/>
+            <input type="text" name="last_name" value={formElement.last_name} onChange={handleChange}/><br/>
+            <label for="age">Age:</label><br/>
+            <input type="text" name="age" value={formElement.age} onChange={handleChange}/><br/>
+            <label for="topic">Choose a topic :</label><br/>
+            <select id="topics" name="topic" value={formElement.topic} onChange={handleChange}>
+                <option value="personalContact">Contact for personal affairs</option>
+                <option value="contactForBug">Contact for a bug</option>
+                <option value="contactForrecruitment">Contact for Recruitment</option>
+            </select><br/>
+            <label for="note">Note:</label><br/>
+            <textarea name="note" rows="10" cols="30" value={formElement.note} onChange={handleChange}/><br/>
+            <button type="submit">Submit</button>
+        </form>                
+    );
+};
