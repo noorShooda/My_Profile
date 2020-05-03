5dd405617b666b4402ef226765716232ba8b4a4c
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-03 16:19:51 +0200

    fix: change label attribute from "for" to "htmlFor" to adapt react logic,and add Ids to fix the lable bug.

--- src/contact/contactForm/Form.js
+++ src/contact/contactForm/Form.js
@@ -32,20 +32,20 @@ export const Form = () => {
 
     return(
         <form className="formStyle" onSubmit={handleSubmitForm }>
-            <label for="fname">First name:</label><br/>
-            <input type="text" name="name" value={formElement.name} onChange={handleChange}/><br/>
-            <label for="lname">Last name:</label><br/>
-            <input type="text" name="last_name" value={formElement.last_name} onChange={handleChange}/><br/>
-            <label for="age">Age:</label><br/>
-            <input type="text" name="age" value={formElement.age} onChange={handleChange}/><br/>
-            <label for="topic">Choose a topic :</label><br/>
+            <label htmlFor="name">First name:</label><br/>
+            <input type="text" id="name" name="name" value={formElement.name} onChange={handleChange}/><br/>
+            <label htmlFor="last-name">Last name:</label><br/>
+            <input type="text" id="last-name" name="last_name" value={formElement.last_name} onChange={handleChange}/><br/>
+            <label htmlFor="age">Age:</label><br/>
+            <input type="text" id="age"name="age" value={formElement.age} onChange={handleChange}/><br/>
+            <label htmlFor="topics">Choose a topic :</label><br/>
             <select id="topics" name="topic" value={formElement.topic} onChange={handleChange}>
                 <option value="personalContact">Contact for personal affairs</option>
                 <option value="contactForBug">Contact for a bug</option>
                 <option value="contactForrecruitment">Contact for Recruitment</option>
             </select><br/>
-            <label for="note">Note:</label><br/>
-            <textarea name="note" rows="10" cols="30" value={formElement.note} onChange={handleChange}/><br/>
+            <label htmlFor="note">Note:</label><br/>
+            <textarea id="note" name="note" rows="10" cols="30" value={formElement.note} onChange={handleChange}/><br/>
             <button type="submit">Submit</button>
         </form>                
     );
